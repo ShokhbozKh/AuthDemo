bf13c9af50f0d2ea8812069bee674e7f6c8d05bd
Shohboz Xurramov <shohboztuit@gmail.com>
2025-07-20 16:26:34 +0500

    simple auth

---
+++ AuthDemo/Controllers/RegisterController.cs
@@ -0,0 +1,47 @@
+﻿using AuthDemo.Data;
+using AuthDemo.DTOs;
+using AuthDemo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Generators;
+
+namespace AuthDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class RegisterController : ControllerBase
+    {
+        private AppDbContext _dbContext;
+        public RegisterController(AppDbContext appDbContext)
+        {
+            _dbContext = appDbContext;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Registers([FromBody] RegisterDto registerDto)
+        {
+            if(_dbContext.Users.Any(u=>u.FirstName==registerDto.FirstName))
+            {
+                return BadRequest($"Foydalunuvchi bor, { registerDto.FirstName}");
+            }
+            var passwordHashcode = HashCode.Combine(registerDto.Password);
+
+            var newUser = new User
+            {
+                FirstName = registerDto.FirstName,
+                LastName = registerDto.LastName,
+                Email = registerDto.Email,
+                PhoneNumber = registerDto.PhoneNumber,
+                Password = passwordHashcode.ToString(),
+            };
+            _dbContext.Users.Add(newUser);
+            _dbContext.SaveChanges();
+
+
+
+            return Ok("Ruyxatdan muvofaqiyatli O'tdingiz!!");
+        }
+           
+
+    }
+}
---
+++ AuthDemo/DTOs/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthDemo.DTOs
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; } = null!;
+        public string PhoneNumber { get; set; } = string.Empty;
+    }
+}
